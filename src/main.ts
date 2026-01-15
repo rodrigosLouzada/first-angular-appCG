9b55e969ee580fc6eda610e2a60bbe4f12772158
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2026-01-15 18:04:17 -0300

    creating user component, incomplete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { HeaderComponent } from './app/header.component';
+import { HeaderComponent } from './header/header.component';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
