a353dccb9e1bc847734ca8520b6246bd7f2b66ea
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2026-01-16 16:47:33 -0300

    user component logic

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const radomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,6 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
-
+  selectedUser = DUMMY_USERS[radomIndex];
+                        
 }
