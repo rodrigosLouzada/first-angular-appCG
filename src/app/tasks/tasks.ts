52f43b24148d5c8f735eb43eab5dd3dd9b0da821
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2026-02-02 16:09:37 -0300

    task component, some false data

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
