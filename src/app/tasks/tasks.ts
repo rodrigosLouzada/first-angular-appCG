33213e3b634f5d09cf7432a1f744830733fccfbb
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2026-02-02 10:23:36 -0300

    typing outputs

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -7,6 +7,6 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tasks.css',
 })
 export class TasksComponent {
-  @Input({required: true}) name!: string
+  @Input() name?: string
 
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -8,18 +8,19 @@ import { Component, computed, EventEmitter, Input, Output, output } from '@angul
   styleUrl: './user.css',
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
-  
+  @Input({required: true}) user! : {
+    id : string;
+    avatar : string;
+    name : string;
+  };
   
   @Output() select = new EventEmitter();
 
   get imagePath(){
-    return '/users/' + this.avatar;
+    return '/users/' + this.user.avatar;
   }
 
   onSelectedUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
