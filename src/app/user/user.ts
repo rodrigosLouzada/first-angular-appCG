053b9adc7e557246d8a9fb9750e19da52403516e
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2026-02-04 10:34:20 -0300

    minor changes

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,5 @@
 import { Component, computed, EventEmitter, Input, Output, output } from '@angular/core';
-
-
-interface User{
-  id : string;
-  avatar : string;
-  name : string;
-}
+import { type User} from './user.model';
 
 @Component({
   selector: 'app-user',
