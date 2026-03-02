3401fc9f54d595d5212724991c0e24302b37ffff
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2026-03-02 15:49:26 -0300

    catching data from users field using two way binding ngmodel

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css',
 })
 export class NewTask {
 
 @Output() cancel = new EventEmitter<void>();
+enteredTitle = '';
+enteredSummary= '';
+enteredDate = '';
 
 onCancel(){
   this.cancel.emit();
